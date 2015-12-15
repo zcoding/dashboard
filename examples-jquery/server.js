c8bddccd9cf250f08e4d8520021704d01400f758
zcoding <wuzijie_1991@163.com>
2015-12-15 17:06:58 +0800

    update buttons

---
+++ examples-jquery/server.js
@@ -0,0 +1,35 @@
+/* This is a simple server for dashboard-jquery examples */
+'use strict';
+const Koa   = require('koa');
+const _     = require('koa-route');
+const Serve = require('koa-static');
+const Path  = require('path');
+const Fs    = require('fs');
+const Chalk = require('chalk');
+const Open  = require('open');
+
+let App = Koa();
+
+App.use(Serve(Path.resolve(__dirname, './public')));
+App.use(Serve(Path.resolve(__dirname, '../build')));
+
+App.use(_.get('/', function *(next) {
+
+  this.type = 'text/html';
+  this.body = Fs.createReadStream(Path.resolve(__dirname, './views/index.html'));
+
+}));
+
+App.use(_.get('/buttons', function *(next) {
+
+  this.type = 'text/html';
+  this.body = Fs.createReadStream(Path.resolve(__dirname, './views/buttons.html'));
+
+}));
+
+const port = 10241;
+
+App.listen(port, () => {
+  console.log(Chalk.green(`running at ${port}...`));
+  Open(`http://localhost:${port}`);
+});
