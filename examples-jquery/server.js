9e4f97a14c0b41e1095e961da06c4b84095f9ea2
zcoding <wuzijie_1991@163.com>
2015-12-16 15:07:43 +0800

    random data by mock.js

--- examples-jquery/server.js
+++ examples-jquery/server.js
@@ -8,6 +8,7 @@ const Fs    = require('fs');
 const Chalk = require('chalk');
 const Open  = require('open');
 const Jade  = require('jade');
+const Mock  = require('mockjs');
 
 let App = Koa();
 
@@ -22,7 +23,29 @@ App.use(_.get('/', function *(next) {
 
 }));
 
-let pages = ['button', 'table', 'form', 'panel', 'grid', 'color'];
+App.use(_.get('/table', function *(next) {
+
+  var render = Jade.compileFile(Path.resolve(__dirname, `./views/table.jade`), {});
+
+  var data = Mock.mock({
+    // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+    'list|10': [{
+      // 属性 id 是一个自增数，起始值为 1，每次增 1
+      'id|+1': 1,
+      'name': '@name',
+      'email': '@email',
+      'age': '@integer(22,33)',
+      'address': '@area',
+      'lastLoginTime': '@datetime'
+    }]
+  })
+
+  this.type = 'text/html';
+  this.body = render(data);
+
+}));
+
+let pages = ['button', 'form', 'panel', 'grid', 'color'];
 
 pages.forEach((page) => {
 
