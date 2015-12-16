aa1eee7b6121e9c7ccd51262ebb1517154ef3ff4
zcoding <wuzijie_1991@163.com>
2015-12-16 15:28:27 +0800

    bordered table

--- examples-jquery/server.js
+++ examples-jquery/server.js
@@ -28,9 +28,7 @@ App.use(_.get('/table', function *(next) {
   var render = Jade.compileFile(Path.resolve(__dirname, `./views/table.jade`), {});
 
   var data = Mock.mock({
-    // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
-    'list|10': [{
-      // 属性 id 是一个自增数，起始值为 1，每次增 1
+    'list|5': [{
       'id|+1': 1,
       'name': '@name',
       'email': '@email',
